814a5bb3ff33d7cfa21bc743e68e3140ce035c83
nico <the.vegeta@gmail.com>
2013-02-06 15:26:57 +0100

    Fix delete command.

--- putio2/putio2.py
+++ putio2/putio2.py
@@ -10,11 +10,11 @@ Usage:
 
     import putio2
     client = putio2.Client('..oauth token here...')
-    
+
     # list files
     files = client.File.list()
     ...
-    
+
     # add a new transfer
     client.Transfer.add('http://example.com/good.torrent')
 
@@ -37,12 +37,12 @@ AUTHENTICATION_URL  = 'https://api.put.io/v2/oauth2/authenticate'
 
 
 class AuthHelper(object):
-    
+
     def __init__(self, client_id, client_secret, redirect_uri):
         self.client_id = client_id
         self.client_secret = client_secret
         self.callback_url = redirect_uri
-    
+
     def get_authentication_url(self):
         params = {
             'client_id': self.client_id,
@@ -51,7 +51,7 @@ class AuthHelper(object):
         }
         query_str = urlencode(query)
         return AUTHENTICATION_URL + "?" + query_str
-    
+
     def get_access_token(self, code):
         params = {
             'client_id': self.client_id,
@@ -66,7 +66,7 @@ class AuthHelper(object):
 
 
 class Client(object):
-    
+
     def __init__(self, access_token):
         self.access_token = access_token
 
@@ -76,7 +76,7 @@ class Client(object):
         attributes = {'client': self}
         self.File     = type('File',     (_File,),     attributes)
         self.Transfer = type('Transfer', (_Transfer,), attributes)
-    
+
     def request(self, path, method='GET', params=None, data=None, files=None, headers=None, raw=False):
         '''
         Wrapper around requests.request()
@@ -90,42 +90,42 @@ class Client(object):
         if not params:
             params = {}
         params['oauth_token'] = self.access_token
-        
+
         url = API_URL + path
         logger.debug('url: %s', url)
-        
+
         r = requests.request(method, url, params=params, data=data, files=files, headers=headers, allow_redirects=True)
         logger.debug('response: %s', r)
-        
+
         if raw:
             return r
-        
+
         logger.debug('content: %s', r.content)
         try:
             r = json.loads(r.content)
-        except ValueError:            
+        except ValueError:
             raise Exception('Server didn\'t send valid JSON:\n%s\n%s' % (r, r.content))
-        
+
         if r['status'] == 'ERROR':
             raise Exception(r['error_type'])
-        
+
         return r
 
 
 class _BaseResource(object):
-    
+
     def __init__(self, resource_dict):
         '''Construct the object from a dict'''
-        
+
         self.__dict__.update(resource_dict)
         try:
             self.created_at = iso8601.parse_date(self.created_at)
         except:
             pass
-    
+
     def __str__(self):
         return self.name.encode('utf-8')
-        
+
     def __repr__(self):
         try:
             # shorten name for display
@@ -133,10 +133,10 @@ class _BaseResource(object):
             return '%s(id=%s, name="%s")' % (self.__class__.__name__, self.id, str(self))
         except:
             return object.__repr__()
-        
+
 
 class _File(_BaseResource):
-        
+
     @classmethod
     def list(cls, parent_id=0, as_dict=False):
         d = cls.client.request('/files/list', params={'parent_id': parent_id})
@@ -146,7 +146,7 @@ class _File(_BaseResource):
             ids = [f.id for f in files]
             return dict(zip(ids, files))
         return files
-    
+
     @classmethod
     def upload(cls, path, name):
         f = open(path)
@@ -155,34 +155,35 @@ class _File(_BaseResource):
         f.close()
         f = d['file']
         return cls(f)
-    
+
     @property
     def files(self):
         '''Helper function for listing inside of directory'''
         return self.list(parent_id=self.id)
-    
+
     def download(self, dest='.', range=None):
         if range:
             headers = {'Range': 'bytes=%s-%s' % range}
         else:
             headers = None
-            
+
         r = self.client.request('/files/%s/download' % self.id, raw=True, headers=headers)
-        
+
         if range:
             return r.content
-            
+
         filename = re.match('attachment; filename\="(.*)"', r.headers['Content-Disposition']).groups()[0]
         with open(os.path.join(dest, filename), 'wb') as f:
             for data in r.iter_content():
                 f.write(data)
 
     def delete(self):
-        return self.client.request('/files/%s/delete' % self.id)
+        file_id = {'file_ids': self.id}
+        return self.client.request('/files/delete', method='POST', data=file_id)
 
 
 class _Transfer(_BaseResource):
-        
+
     @classmethod
     def list(cls, parent_id=0, as_dict=False):
         d = cls.client.request('/transfers/list')
@@ -192,7 +193,7 @@ class _Transfer(_BaseResource):
             ids = [t.id for t in transfers]
             return dict(zip(ids, transfers))
         return transfers
-    
+
     @classmethod
     def add(cls, url, parent_id=0, extract=False):
         d = cls.client.request('/transfers/add', method='POST', data=dict(
